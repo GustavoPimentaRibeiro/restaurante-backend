78f7ec93f044a0a68115eb41bcdc0ecae153c499
Gustavo Pimenta Ribeiro <63890172+GustavoPimentaRibeiro@users.noreply.github.com>
2023-11-25 09:44:12 -0300

    Configuracoes padroes

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import * as express from "express"
+import { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!")
+})
+
+app.listen(3000, () => console.log("Listening on port 3000!"))
