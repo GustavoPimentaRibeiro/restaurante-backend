538320eb62c2100a26e252640f6e07d43412abbc
Gustavo Pimenta Ribeiro <63890172+GustavoPimentaRibeiro@users.noreply.github.com>
2023-12-16 10:50:19 -0300

    feat: adding Promotion model

---
+++ src/models/Promotion.ts
@@ -0,0 +1,40 @@
+import { DataTypes } from "sequelize";
+import db from "@/database/db";
+import Product from "@/models/Product";
+
+const Promotion = db.define(
+  "promotions",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(80),
+      allowNull: false,
+    },
+    promotional_price: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    valid_days: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: Product,
+        key: "id",
+      },
+    },
+  },
+  {
+    timestamps: false,
+  },
+);
+
+export default Promotion;
